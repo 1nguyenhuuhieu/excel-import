e26382114c6b74a4897e7fec0150416483157d83
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-11-17 00:28:58 +0700

    ssss

--- covid19/convertexcel/forms.py
+++ covid19/convertexcel/forms.py
@@ -1,8 +1,19 @@
 from django.forms import ModelForm
+from django import forms
 from .models import *
 
 class FileUploadForm(ModelForm):
     class Meta:
         model = FileUpload
         fields = ['excel_file']
+        widgets = {
+            'excel_file': forms.FileInput(
+                attrs = {
+                    'class': 'form-control form-control-lg mt-3'
+                }
+            )
+        }
+        labels = {
+            'excel_file': ''
+        }
         
