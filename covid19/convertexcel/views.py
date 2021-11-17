13371cd357d2a3c2325b08273be174fb356f563c
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2021-11-17 14:49:01 +0700

    Update views.py

--- covid19/convertexcel/views.py
+++ covid19/convertexcel/views.py
@@ -145,10 +145,10 @@ def view(request, id=0):
                 clean_phone = clean_phone.replace(".", "")
                 clean_phone = clean_phone.replace(",", "")
                 clean_phone = clean_phone.replace(" ", "")
-                string_phone = "0000" + clean_phone
-                text_phone = string_phone[-10:]
             else:
-                text_phone = str(excel_phone)
+                string_phone = str(excel_phone)
+            string_phone = "0000" + clean_phone
+            text_phone = string_phone[-10:]
         else:
             text_phone = "0999999999"
 
